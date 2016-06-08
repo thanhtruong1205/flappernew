ae7e448518a07e59f076853be9ff310bedddeab1
thanhtruong1205 <thanhtruongpu@gmail.com>
2016-06-08 19:15:21 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,80 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+/* require mongoose schema must before routes */
+var mongoose = require('mongoose');
+var passport = require('passport');
+
+require('./models/Posts');
+require('./models/Comments');
+require('./models/Users');
+require('./config/passport');
+
+mongoose.connect('mongodb://localhost/news');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
+app.use(passport.initialize());
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+/* Define path to bootstrap, angularjs, uirouter script */
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/fonts', express.static(__dirname + '/node_modules/bootstrap/dist/fonts'));
+app.use('/js', express.static(__dirname + '/node_modules/angular'));
+app.use('/js', express.static(__dirname + '/node_modules/angular-ui-router/release'));
+
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
